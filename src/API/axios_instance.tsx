5d8663fe16d6cd2bd596eb1967e7b2339e0fd166
yuvraj satish lolage <yuvraj.lolage7@gmail.com>
2026-03-09 12:45:22 +0530

    added hosted backend link

--- src/API/axios_instance.tsx
+++ src/API/axios_instance.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:3000",
+    // baseURL:"http://localhost:3000",
+    baseURL:"https://skoda-self-leaning-kit-backend-production.up.railway.app",
     headers:{
         "Content-Type":"application/json"
 
